7f99667dfce396c910556a745c1281f187810120
GeorgeCloud <net.george.sky@gmail.com>
2016-10-24 20:53:11 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,12 @@
-print 'sdf'
+## linkedList
+
+class Node:
+    def __init__(self, data, next=None)
+        self.data = data
+        self.next = next
+
+
+
+class LinkedList:
+    def __init__(self):
+        pass
